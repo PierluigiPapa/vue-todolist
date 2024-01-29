12ad3afc7bd6391d54c652541127004901833fb9
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-29 15:28:17 +0100

    feat: add "lista" on main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,6 +3,33 @@ const {createApp} = Vue
 createApp ({
     data () {
         return {
+            lista: [ 
+                {
+                    text: "Fare la spesa al supermercato",
+                    done: "tre",
+                },
+
+                {
+                    text: "Comprare le medicine in farmacia",
+                    done: "false",
+                },
+
+                {
+                    text: "Studiare storia medievale, capitolo 10 e 11",
+                    todo: "true",
+                },
+
+                {
+                    text: "Andare in palestra",
+                    todo: "false",
+                },
+
+                {
+                    text: "Guardare una serie tv su Netflix",
+                    todo: "true",
+                },
+              
+            ]
         }
     }
 }).mount ('#app')
